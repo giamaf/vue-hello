5a0ae2e1cc225164c57efd1cd4174d5a334d197f
giamaf <gianluca.maffucci@hotmail.com>
2024-01-19 17:30:45 +0100

    added vue settings + printed message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('JS OK');
+const { createApp } = Vue
+
+const myApp = createApp({
+    data() {
+        return {
+            textStyle: ['text-center', 'p-3'],
+            message: 'Hello Vue!'
+        }
+    }
+})
+
+myApp.mount('#root');
+
